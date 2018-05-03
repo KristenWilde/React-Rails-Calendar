1c63e855cd71bd0c5789ef32cc4b44b84e4aea62
Kristen Wilde <kristen.wilde@gmail.com>
2018-05-03 16:53:52 -0400

    initial commit

---
+++ app/assets/javascripts/components/appointments.jsx
@@ -0,0 +1,17 @@
+console.log('loaded appointments.jsx')
+
+// var Appointments = React.createClass({
+//   render: function() {
+//     return (
+//       <h1>CalReact</h1>
+//     )
+//   }
+// });
+
+class Appointments extends React.Component {
+  render() {
+  return (
+    <h1>CalReact</h1>
+      )
+   }
+}
